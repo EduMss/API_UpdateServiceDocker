6d61780b20e0af5b066af06dc4b71aaf613f03fa
EduMss <eduardo555gatinho@hotmail.com>
2024-06-06 12:13:36 -0300

    update

--- UpdateServiceDocker/Controllers/UpdateServiceDockerController.cs
+++ UpdateServiceDocker/Controllers/UpdateServiceDockerController.cs
@@ -37,9 +37,12 @@ namespace UpdateServiceDocker.Controllers
                 }
 
                 string Script = configPorta.dirScript;
+                //return Ok($"Achado: {ditScripts}{Script}");
 
+                //ExecuteCommand($"{ditScripts}{Script}");
                 Run($"{ditScripts}{Script}", false);
-                return Ok($"Comando executado!");
+
+		return Ok($"Comando executado!");
             }
             catch (Exception ex)
             {
@@ -69,44 +72,78 @@ namespace UpdateServiceDocker.Controllers
         }
 
 
-        public static string Run(string cmd, bool sudo = false)
+        private string ExecuteCommand(string command)
         {
-            try
+            var processStartInfo = new ProcessStartInfo
             {
-                var psi = new ProcessStartInfo
+                FileName = "/host/bash",
+                //Arguments = $"-c \"{command}\"",
+                //Arguments = "mkdir /Files-Data/novo",
+		Arguments = $"-c \"/host/docker ps -a\"",
+		RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (var process = new Process { StartInfo = processStartInfo })
+            {
+                process.Start();
+                string result = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                if (!string.IsNullOrEmpty(error))
                 {
-                    FileName = "/bin/bash",
-                    Arguments = $"-c \"{(sudo ? "sudo " : "")}{cmd}\"",
-                    RedirectStandardOutput = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true,
-                    RedirectStandardError = true,
-                    RedirectStandardInput = true,
-                };
-
-                using (Process proc = new Process { StartInfo = psi })
-                {
-                    proc.Start();
-
-                    string output = proc.StandardOutput.ReadToEnd();
-                    string error = proc.StandardError.ReadToEnd();
-                    proc.WaitForExit();
-
-                    if (!string.IsNullOrWhiteSpace(error))
-                    {
-                        Console.WriteLine($"Error: {error}");
-                        return $"Error: {error}";
-                    }
-
-                    Console.WriteLine($"Output: {output}");
-                    return output;
+                    throw new Exception($"Error: {error}");
                 }
-            }
-            catch (Exception exc)
-            {
-                Console.WriteLine($"Command '{cmd}' failed: {exc.Message}");
-                return $"Exception: {exc.Message}";
+
+                return result;
             }
         }
+
+public static string Run(string cmd, bool sudo = false)
+    {
+        try
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "/bin/bash",
+                //FileName = "/host/bash",
+		Arguments = $"-c \"{(sudo ? "sudo " : "")}{cmd}\"",
+                //Arguments = "mkdir /app/Files-Data/novo",
+		//Arguments = $"-c \"/host/docker ps -a\"",
+		RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+            };
+
+            using (Process proc = new Process { StartInfo = psi })
+    	    {
+            proc.Start();
+
+            string output = proc.StandardOutput.ReadToEnd();
+            string error = proc.StandardError.ReadToEnd();
+            proc.WaitForExit();
+
+            if (!string.IsNullOrWhiteSpace(error))
+            {
+                Console.WriteLine($"Error: {error}");
+                return $"Error: {error}";
+            }
+
+            Console.WriteLine($"Output: {output}");
+            return output;
+            }
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Command '{cmd}' failed: {exc.Message}");
+        return $"Exception: {exc.Message}";
+    }
+    }
+
     }
 }
